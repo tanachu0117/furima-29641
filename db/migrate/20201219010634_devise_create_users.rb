524dbab9c798f40fe274c8c84622c48615229441
tanachu0117 <71695953+tanachu0117@users.noreply.github.com>
2020-12-19 12:56:28 +0900

    名前　仮名カラム追加

--- db/migrate/20201219010634_devise_create_users.rb
+++ db/migrate/20201219010634_devise_create_users.rb
@@ -4,12 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-      t.string :first_name,         null: false
-      t.string :last_name,          null: false
-      t.date   :birthday,           null: false
+      t.string :name,                                  null: false
+      t.string :email,                                 null: false, default: ""
+      t.string :encrypted_password,                    null: false, default: ""
+      t.string :first_name,                            null: false
+      t.string :last_name,                             null: false
+      t.date   :birthday,                              null: false
+      t.string :first_name_paseudnym_readeing,         null: false
+      t.string :last_name_paseudnym_readeing,          null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_12_19_010634) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.date "birthday", null: false
+    t.string "first_name_paseudnym_readeing", null: false
+    t.string "last_name_paseudnym_readeing", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
