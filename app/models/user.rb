89ce9820a0625930f7fb6511e52f03095d841ac1
tanachu0117 <71695953+tanachu0117@users.noreply.github.com>
2020-12-19 12:51:30 +0900

    バリデーション・CSS追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password, length: {minimum: 6}
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
 end
