2c388d67e27bde3015eba1a74ba1c65cec23a0d8
tanachu0117 <71695953+tanachu0117@users.noreply.github.com>
2020-12-20 21:13:16 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,19 @@
 class Item < ApplicationRecord
-  validates :name             , presence: true
-  validates :detail           , presence: true
-  validates :category_id      , presence: true
-  validates :item_state_id    , presence: true
-  validates :shipping_fee_id  , presence: true
-  validates :prefecture_id    , presence: true
-  validates :shipping_days_id , presence: true
-  validates :price            , presence: true
-  validates :image            , presence: true
+
   with_options presence: true do
-    validates :category_id      , numericality: { other_than: 0 }
-    validates :item_state_id    , numericality: { other_than: 0 } 
-    validates :shipping_fee_id  , numericality: { other_than: 0 } 
-    validates :prefecture_id    , numericality: { other_than: 0 } 
-    validates :shipping_days_id , numericality: { other_than: 0 } 
+    validates :category_id        , numericality: { other_than: 0 }
+    validates :item_state_id      , numericality: { other_than: 0 } 
+    validates :shipping_fee_id    , numericality: { other_than: 0 } 
+    validates :prefecture_id      , numericality: { other_than: 0 } 
+    validates :shipping_days_id   , numericality: { other_than: 0 }
+    validates :name                
+    validates :detail               
+    validates :image                
+    validates :price              , format: { with: /\A[0-9]+\z/ }
+    validates :price              , numericality: { greater_than: 300 }
+    validates :price              , numericality: { less_than: 9999999 }
+    validates :price              , numericality: { only_integer: true}
   end
-  validates :price              , format: { with: /\A[0-9]+\z/ }
-  validates :price              , numericality: { greater_than: 300 }
-  validates :price              , numericality: { less_than: 9999999 }
-  validates :price              , numericality: { only_integer: true}
 
   belongs_to :user
   has_one_attached :image  
