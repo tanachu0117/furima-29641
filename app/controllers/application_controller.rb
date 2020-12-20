29ee1e01195f2a059d0c7f499e31a4d0b3d9070b
tanachu0117 <71695953+tanachu0117@users.noreply.github.com>
2020-12-20 20:43:43 +0900

    before_action :authenticate_user! 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
-  
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name, :first_name_paseudnym_readeing, :last_name_paseudnym_readeing, :birthday])
