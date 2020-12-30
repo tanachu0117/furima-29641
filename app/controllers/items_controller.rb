b188df7c2cc72fd81756211ab2afffd4a33ba867
tanachu0117 <71695953+tanachu0117@users.noreply.github.com>
2020-12-30 20:44:53 +0900

    destory実行のif条件

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :sign_in_check, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :sign_in_check, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -34,11 +34,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
     redirect_to root_path
   end
-
 
   private
 
