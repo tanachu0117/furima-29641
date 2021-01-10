dd5fd5707911b61a769fe2d07404cebcb5ed8cf6
tanachu0117 <71695953+tanachu0117@users.noreply.github.com>
2021-01-10 15:21:31 +0900

    テーブル保存設定

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -17,4 +17,14 @@ class UserDonation
     # 「寄付金」の金額範囲に関するバリデーション
     validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: "is out of setting range"}
   end
+  def save
+    # ユーザーの情報を保存し、「user」という変数に入れている
+    user = User.create
+    # 住所の情報を保存
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name,user_id: user.id)
+    # 寄付金の情報を保存
+    Donation.create(price: price, user_id: user.id)
+  end
+
+
 end
