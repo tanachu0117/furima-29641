e2c231ede3a11bd31b004789b19be0dd014c1805
tanachu0117 <71695953+tanachu0117@users.noreply.github.com>
2021-01-10 15:08:35 +0900

    attr_accessor設定

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,3 +1,3 @@
 class UserDonation
   include ActiveModel::Model
-  attr_accessor :name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price
+  attr_accessor :card_nmuber, :expiration_date_manth, :expiration_date_year, :security_number, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number
