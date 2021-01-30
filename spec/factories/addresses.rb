32b7c9ee3104bd94f9771eaf5d071b4f0a06b521
tanachu0117 <71695953+tanachu0117@users.noreply.github.com>
2021-01-30 16:07:58 +0900

    addressテストコード準備

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
-    
+    postal_code                   {Faker::Name.initials(number: 2)}
+    prefecture_id                 {"1"}
+    city                          {Faker::Address.city}
+    city_number                   {"1-3"}
+    building_name                 {"梅田ビル"}
+    phone_number                  {"0900000000"}
+    order_id                      {"1"}
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  @address = FactoryBot.build(:user)
+  end
+
+  describe '配送先入力' do
+    it "phone_number,postal_code,prefacture_idが存在すれば登録できる" do
+      expect(@address).to be_valid
+    end
+    
+  end
 end
