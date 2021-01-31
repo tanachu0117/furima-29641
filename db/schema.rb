904372c78077152d246f5dcc61512dd15f4e4f47
tanachu0117 <71695953+tanachu0117@users.noreply.github.com>
2021-01-31 10:56:12 +0900

    建物名　は必須項目から外す

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_062321) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "city_number", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
